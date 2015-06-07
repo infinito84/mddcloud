fb20d5b9ca328321dbb2aa26a9e965ef42ca1be4
Sergio Andrés Ñustes <infinito84@gmail.com>
2015-06-07 00:48:16 -0500

    Increasing max pool size

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -2,7 +2,7 @@ var mongoose 	= require('mongoose'),
 	config 		= require('../app/config');
 	
 module.exports=(function(){
-	mongoose.connect(config.mongodb);
+	mongoose.connect(config.mongodb, { server: { poolSize: 100 }});
 	
 	//require("./testData") //For Development
 	
